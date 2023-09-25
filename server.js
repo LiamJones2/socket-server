d0264750928ad213045c5cdd973d378e3a6ca586
liamjones2 <liammjoness@gmail.com>
2023-09-25 11:50:39 +0100

    changes to chat message

--- server.js
+++ server.js
@@ -6,47 +6,58 @@ const { join } = require('path');
 const cors = require('cors');
 
 
-const activeUsers = new Map(); 
+const activeUsers = new Map();
 
 const server = createServer(app);
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "*", 
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+const users = {};
+
+io.on('connection', (socket) => {
+  socket.on('join', (user) => {
+    console.log(user)
+    socket.join(user);
+    users[socket.id] = user;
+    console.log(users)
   });
-  
-  
-  app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-  });
-  
-  // Store user socket information
-  const users = {};
-  
-  io.on('connection', (socket) => {
-    socket.on('join', (room) => {
-      room = 113
-      console.log(room)
-      socket.join(room);
-      users[socket.id] = room; 
-      socket.broadcast.to(room).emit('user-connected', { userId: socket.id, message: 'New user has connected' });
-    });
-  
-    socket.on('chat message', (msg) => {
-      console.log(msg)
-      const room = users[socket.id];
-      socket.broadcast.to(room).emit('chat message', msg);
-      socket.broadcast.to(room).emit('notification', msg)
-    });
-  
-    socket.on('disconnect', () => {
-      const room = users[socket.id];
-      socket.leave(room);
-      delete users[socket.id];
+
+  socket.on('chat message', (msg) => {
+    Object.entries(users).forEach(([socketId, user]) => {
+      if (user === notification.to) {
+        io.to(socketId).emit('chat message', msg);
+      }
     });
   });
 
-  const PORT = 8080;
-  server.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+  socket.on('notification', (notification) => {
+    if (notification.type === "msg") {
+
+      Object.entries(users).forEach(([socketId, user]) => {
+        if (user === notification.to) {
+          io.to(socketId).emit('notification', `You have received a message from${notification.from}`);
+        }
+      });
+    }
+    // socket.broadcast.to(user).emit('notification', msg);
+  });
+
+  socket.on('disconnect', () => {
+    const room = users[socket.id];
+    socket.leave(room);
+    delete users[socket.id];
+  });
+});
+
+const PORT = 8080;
+server.listen(PORT, () => console.log(`Listening on ${PORT}...`));
